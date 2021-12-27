0e885146a2691fe3ea55fd8e007974a463dab2c8
IsabelleHuyghe <isabelle.huyghe@ugent.be>
2021-12-27 20:38:52 +0100

    emailoptin2

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="https://2d1474ad.sibforms.com/serve/MUIEABkaJorUmvR6Nitb_DEWT_ZZKfRBvcjyxn0VrFPbwJSpzjmvq5zgAn7tehNRZbkCRWNdOStpFZeak5H0VGQbv9R8dhIxwSS56jonpgQC7Es96P-2JhuiSiwmrRUsyqmps4FvfKqts4DV3o3Guug52pVJ5MemclMHbko6lylqqR_erbLg28piVdgLz8IsyaLLDzejmoKUw5fl" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+      {{formstate&&<iframe width="540" height="900" src="https://2d1474ad.sibforms.com/serve/MUIEABkaJorUmvR6Nitb_DEWT_ZZKfRBvcjyxn0VrFPbwJSpzjmvq5zgAn7tehNRZbkCRWNdOStpFZeak5H0VGQbv9R8dhIxwSS56jonpgQC7Es96P-2JhuiSiwmrRUsyqmps4FvfKqts4DV3o3Guug52pVJ5MemclMHbko6lylqqR_erbLg28piVdgLz8IsyaLLDzejmoKUw5fl" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>}
     </div>
   );
 };
