3eebfeeac3344b31be466e6f5338997bb45ea6a7
sofievvlier <sofie.vanvlierberghe@ugent.be>
2021-12-28 18:37:22 +0100

    def

--- pages/_document.js
+++ pages/_document.js
@@ -32,7 +32,7 @@ export default class MyDocument extends Document {
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/XXXXXXXXXXXXXXXXXXXXXXXX/XXXXXXXXX';
+            s1.src='https://embed.tawk.to/61c8b5f780b2296cfdd3de8a/default';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
