02893d09ae0e3cc2925d417c633dd5c37cb828db
sofievvlier <sofie.vanvlierberghe@ugent.be>
2022-01-01 20:55:19 +0100

    pogin ...

--- pages/_document.js
+++ pages/_document.js
@@ -22,12 +22,12 @@ export default class MyDocument extends Document {
           `
             }}
           />
-          {/* <script dangerouslySetInnerHTML={{
+          {<script dangerouslySetInnerHTML={{
             __html: ` (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('${process.env.NEXT_PUBLIC_LEADFEEDER}');`
           }}
-          /> */}
+          /> }
 
-          {/* <script dangerouslySetInnerHTML={{
+          {<script dangerouslySetInnerHTML={
             __html: `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -36,8 +36,8 @@ export default class MyDocument extends Document {
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
-          }}
-          /> */}
+          }
+          />} 
 
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1446177920850529"
             crossorigin="anonymous"></script>
