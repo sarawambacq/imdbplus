8b539438d6b1fb09f37ef7b49259466180c7c710
sarawambacq <94790251+sarawambacq@users.noreply.github.com>
2022-01-02 21:09:25 +0100

    Update _document.js
    fix }

--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
           }}
           /> }
 
-          {<script dangerouslySetInnerHTML={
+          {/* <script dangerouslySetInnerHTML={{            
             __html `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -36,8 +36,8 @@ export default class MyDocument extends Document {
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
-          }
-          />} 
+          }}
+          /> */}
 
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1446177920850529"
             crossorigin="anonymous"></script>
